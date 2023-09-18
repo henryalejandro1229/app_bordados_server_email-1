d86cc2aac7dd61d54762c713c323e45c9e49b92c
Henry Alejandro <henryalejandro1229@gmail.com>
2023-09-18 16:40:32 -0600

    Fix - host

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const userEmail = "empresa@sportstore.proyectowebuni.com";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp.hostinger.com",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
